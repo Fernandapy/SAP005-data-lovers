1b345a56b99077ec9f8d444534e0500a498c7ccf
Fernanda Santos <fsantos@NSCYBER.LOCAL>
2020-12-05 16:58:55 -0300

    teste feito

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,177 @@
-import { example, anotherExample } from '../src/data.js';
 
+import { statusFilter, speciesFilter, genderFilter, searchLocation } from '../src/test.js';
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+const resultadoTest= [
+  
+    {
+        "id": 1,
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "species": "Human",
+        
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        
+        
+        
+    },
+    {
+        "id": 2,
+        "name": "Morty Smith",
+        "status": "Alive",
+        "species": "Human",
+        
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        
+    },
+    {
+        "id": 3,
+        "name": "Summer Smith",
+        "status": "Alive",
+        "species": "Human",
+       
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+      },
+    {
+        "id": 4,
+        "name": "Beth Smith",
+        "status": "Alive",
+        "species": "Human",
+        
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+      },
+    {
+        "id": 5,
+        "name": "Jerry Smith",
+        "status": "Alive",
+        "species": "Human",
+       
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+      }
+  
+      ];
+      
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
+describe('statusFilter', () => {
+it('is a function', () => {
+  expect(typeof statusFilter).toBe('function');
 });
 
+it('returns `statusFilter`', () => {
+  expect(statusFilter(resultadoTest, 'Alive')).toStrictEqual([
+    {
+      "id": 1,
+      "name": "Rick Sanchez",
+      "status": "Alive",
+      "species": "Human",
+      
+      "location": {
+          "name": "Earth (Replacement Dimension)",
+          "url": "https://rickandmortyapi.com/api/location/20"
+      },
+      
+      
+      
+  },
+  {
+      "id": 2,
+      "name": "Morty Smith",
+      "status": "Alive",
+      "species": "Human",
+      
+      "location": {
+          "name": "Earth (Replacement Dimension)",
+          "url": "https://rickandmortyapi.com/api/location/20"
+      },
+      
+  },
+  {
+      "id": 3,
+      "name": "Summer Smith",
+      "status": "Alive",
+      "species": "Human",
+     
+      "location": {
+          "name": "Earth (Replacement Dimension)",
+          "url": "https://rickandmortyapi.com/api/location/20"
+      },
+    },
+  {
+      "id": 4,
+      "name": "Beth Smith",
+      "status": "Alive",
+      "species": "Human",
+      
+      "location": {
+          "name": "Earth (Replacement Dimension)",
+          "url": "https://rickandmortyapi.com/api/location/20"
+      },
+    },
+  {
+      "id": 5,
+      "name": "Jerry Smith",
+      "status": "Alive",
+      "species": "Human",
+     
+      "location": {
+          "name": "Earth (Replacement Dimension)",
+          "url": "https://rickandmortyapi.com/api/location/20"
+      },
+    }
+  ]);
+});
+});
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+describe('speciesFilter', () => {
+it('is a function', () => {
+  expect(typeof speciesFilter).toBe('function');
+});
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+it('returns `example`', () => {
+  expect(example()).toBe('example');
+});
+});
+
+describe('genderFilter', () => {
+it('is a function', () => {
+  expect(typeof genderFilter).toBe('function');
+});
+
+it('returns `example`', () => {
+  expect(example()).toBe('example');
+});
+});
+
+describe('gsearchLocation', () => {
+it('is a function', () => {
+  expect(typeof searchLocation).toBe('function');
+});
+
+it('returns `example`', () => {
+  expect(example()).toBe('example');
+});
+});
+
+describe('statisticData', () => {
+it('is a function', () => {
+  expect(typeof statisticData).toBe('function');
+});
+
+it('returns `example`', () => {
+  expect(example()).toBe('example');
+});
 });
